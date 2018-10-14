3a767d9800cdf8ffd7d6c1fbd3066b199080cf21
RSilviu <1silviu.rusu@gmail.com>
2018-10-14 17:57:44 +0300

    initial defs of solidity constructs: functions, contracts

--- ref_syntax.v
+++ ref_syntax.v
@@ -1,6 +1,7 @@
 Require Import ZArith.
 Require Import String.
 
+Open Scope list_scope.
 
 Inductive aexp :=
 | Int : Z -> aexp
@@ -13,32 +14,108 @@ Inductive aexp :=
 Inductive bexp :=
 | boolean : bool -> bexp
 | aexpEq : aexp -> aexp -> bexp
+| aexpLt : aexp -> aexp -> bexp
+| aexpGt : aexp -> aexp -> bexp
 | aexpLeq : aexp -> aexp -> bexp
 | aexpGeq : aexp -> aexp -> bexp
 | Not : bexp -> bexp
 | And : bexp -> bexp -> bexp.
 
+
+(******************)
+(* Solidity decls *)
+(******************)
+
+Inductive var_decl :=
+| uint : string -> var_decl.
+
+Definition vars_decl := list var_decl.
+
+Let a : string := "a".
+
+Compute uint a.
+
+
 Inductive instr :=
 | Assignment : aexp -> aexp -> instr
 | IfThenElse : bexp -> list instr -> list instr -> instr
 | While : bexp -> list instr -> instr
-| Skip : instr.
+| Skip : instr
+| Return : aexp -> instr.
+
+(************************************)
+(* Solidity functions related stuff *)
+(************************************)
+
+Inductive fn_visibility :=
+| public : fn_visibility
+| private : fn_visibility.
+
+Definition param_decl := var_decl.
+Definition params_decl := list param_decl.
+
+Inductive fn_return :=
+| returns : params_decl -> fn_return.
+
+
+Definition fn_body := list instr.
+
+Inductive fn_decl :=
+| function : string -> params_decl -> option fn_visibility -> fn_return ->
+ fn_body -> fn_decl.
+
+Definition fn_decls := list fn_decl.
+
+Let bid : string := "bid".
+Definition no_params : params_decl := nil.
+Let body : list instr := Skip :: nil.
+Let ret_params := returns (uint "a" :: uint "b" :: nil).
+
+Compute ret_params.
+
+Let bid_fn : fn_decl := function bid no_params (Some public) ret_params body.
+Compute bid_fn.
+
+(* Contracts *)
+(*************)
+
+Inductive contract_body_decl :=
+| contract_fn_decl : fn_decl -> contract_body_decl
+| contract_var_decl: var_decl -> contract_body_decl.
+
+Definition contract_body_decls := list contract_body_decl.
+
+Inductive sol_contract :=
+| contract : string -> contract_body_decls -> sol_contract.
+
+Let contract_body := (contract_fn_decl bid_fn)
+ :: (contract_var_decl (uint "address")) :: nil.
+
+Compute contract_body.
+
+Compute contract "Purchase" contract_body.
+
+
+(*********************************)
+(* End of Solidity related stuff *)
+(*********************************)
 
 Definition Code := list instr.
-Definition Env := aexp -> option Z.
+Definition Env := string -> option Z. 
 Definition emptyEnv : Env := fun x => None.
 (* UTILS *)
 
-Definition update (env : Env) (var : aexp) (value : Z) : aexp -> option Z :=
-  fun x' => match x', var with 
-  | Id x, Id y => if (string_dec x y) then Some value else (env x')
-  | _, _ => None
+Definition update (env : Env) (var : aexp) (value : Z) : string -> option Z :=
+  fun x =>
+  match var with
+  | Id s => if (string_dec x s) then Some value else (env x)
+  | _ => None
   end.
 
 Fixpoint evalaexp (env : Env) (a : aexp) : option Z :=
   match a with
   | Int z => Some z
-  | Id x => (env a)
+  | Id x => (env x)
   | Plus a1 a2 => match (evalaexp env a1), (evalaexp env a2) with
                   | Some v1, Some v2 => Some (Z.add v1 v2)
                   | _, _ => None
@@ -64,10 +141,18 @@ Fixpoint evalbexp (env : Env) (b : bexp) : option bool :=
                     | Some v1, Some v2 => Some (Z.eqb v1 v2)
                     | _, _ => None
                     end
+  | aexpLt a1 a2 => match (evalaexp env a1), (evalaexp env a2) with
+                    | Some v1, Some v2 => Some (Z.ltb v1 v2)
+                    | _, _ => None
+                    end
   | aexpLeq a1 a2 => match (evalaexp env a1), (evalaexp env a2) with
                     | Some v1, Some v2 => Some (Z.leb v1 v2)
                     | _, _ => None
                     end
+  | aexpGt a1 a2 => match (evalaexp env a1), (evalaexp env a2) with
+                    | Some v1, Some v2 => Some (Z.gtb v1 v2)
+                    | _, _ => None
+                     end
   | aexpGeq a1 a2 => match (evalaexp env a1), (evalaexp env a2) with
                     | Some v1, Some v2 => Some (Z.geb v1 v2)
                     | _, _ => None
@@ -81,6 +166,28 @@ Fixpoint evalbexp (env : Env) (b : bexp) : option bool :=
                  | _, _ => None
                  end
   end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
